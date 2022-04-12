728c943b7049491294edd2f153cd2ce63dedb82e
Davey-M <davey.meuer@gmail.com>
2022-04-12 13:50:24 -0500

    add large tab boilerplate

---
+++ src/components/Miscellaneous/LargeTabs/LargeTabs.jsx
@@ -0,0 +1,43 @@
+import { useRef, useState } from 'react';
+
+// tabLabels and tabContents should have corresponding indexes
+function LargeTabs({ tabLabels, tabContents }) {
+  const [selectedTab, setSelectedTab] = useState(0);
+  const tabContentContainer = useRef();
+
+  const setTab = (index) => {
+    tabContentContainer.current.classList.toggle('hidden');
+
+    setTimeout(() => {
+      setSelectedTab(index);
+      tabContentContainer.current.classList.toggle('hidden');
+    }, 200);
+  };
+
+  return (
+    <>
+      <div className='tab-container'>
+        <div className='tab-label-container'>
+          {tabLabels.map((tabLabel, index) => {
+            // tabs set the selected tab on click
+            return (
+              <div
+                className='tab'
+                onClick={() => {
+                  setTab(index);
+                }}
+              >
+                {tabLabel}
+              </div>
+            );
+          })}
+        </div>
+        <div ref={tabContentContainer} className='tab-content-container'>
+          {tabContents[selectedTab]}
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default LargeTabs;
