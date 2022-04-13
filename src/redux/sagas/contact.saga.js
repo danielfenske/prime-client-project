c477224ed5884a85e42503580f6a0c117c821986
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-04-13 14:49:01 -0500

    Built out edit and add contact modals; added and tested add and update functions within the contact saga

--- src/redux/sagas/contact.saga.js
+++ src/redux/sagas/contact.saga.js
@@ -14,8 +14,38 @@ function* getContactList() {
   }
 }
 
+// POST new contact to DB
+function* postContact(action) {
+  const newContact = action.payload;
+
+  try {
+    yield axios.post(`api/contact`, newContact);
+
+    yield put({type: 'FETCH_CONTACT_LIST'});
+  } catch (error) {
+    console.log('Error POSTING contact', error);   
+  }
+}
+
+// UPDATE existing contact within DB
+function* updateContact(action) {
+
+  const contactId = action.payload.id;
+  const updatedContact = action.payload;
+
+  try {
+    yield axios.put(`api/contact/${contactId}`, updatedContact);
+
+    yield put({type: 'FETCH_CONTACT_LIST'});
+  } catch (error) {
+    console.log('Error UPDATING contact', error);
+  }
+}
+
 function* contactSaga() {
   yield takeLatest('FETCH_CONTACT_LIST', getContactList);
+  yield takeLatest('POST_CONTACT', postContact);
+  yield takeLatest('UPDATE_CONTACT', updateContact);
 }
 
 export default contactSaga;
