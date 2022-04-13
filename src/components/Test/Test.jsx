b2ec223745223560783a067572b0e5ee8af3188a
Davey-M <davey.meuer@gmail.com>
2022-04-13 13:51:23 -0500

    add search feature

--- src/components/Test/Test.jsx
+++ src/components/Test/Test.jsx
@@ -2,8 +2,15 @@
 import { useState } from 'react';
 import Modal from '../Miscellaneous/Modal/Modal';
 
+// link to the autocomplete
+// https://mui.com/material-ui/react-autocomplete/
+
 function Test() {
   const [open, setOpen] = useState(false);
+
+  const [search, setSearch] = useState('');
+
+  const words = ['One', 'Two', 'Three', 'Threees'];
 
   return (
     <>
@@ -16,6 +23,24 @@ function Test() {
         >
           Open
         </button>
+
+        {/* input for filtering the list */}
+        <input
+          type='text'
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
+        />
+
+        <ul>
+          {words
+            // filter the list based off the input
+            .filter((e) => {
+              return e.toUpperCase().includes(search.toUpperCase());
+            })
+            .map((word, index) => {
+              return <li key={index}>{word}</li>;
+            })}
+        </ul>
       </div>
 
       <Modal open={open}>
