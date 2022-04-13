c5c93efade692d2b7fd734bf82f93d721aedf92c
Davey-M <davey.meuer@gmail.com>
2022-04-13 11:24:33 -0500

    start admin styling

--- src/components/AdminView/AdminView.jsx
+++ src/components/AdminView/AdminView.jsx
@@ -4,9 +4,15 @@ import { useDispatch, useSelector } from 'react-redux';
 
 import UserCard from './UserCard/UserCard';
 
+import './AdminView.css';
+
 function AdminView() {
   const dispatch = useDispatch();
   const allUsers = useSelector((store) => store.allUsers);
+
+  const addNewUser = () => {
+    console.log('In Add New User');
+  };
 
   useEffect(() => {
     dispatch({ type: 'FETCH_ALL_USERS' });
@@ -14,9 +20,10 @@ function AdminView() {
 
   return (
     <>
-      <div>
-        <Button variant='contained'>Add User</Button>
-        <h1>Hello From the Admin View</h1>
+      <div className='admin-container'>
+        <Button variant='contained' onClick={addNewUser}>
+          Add User
+        </Button>
         <div className='user-card-holder'>
           {allUsers.map((user, index) => {
             return <UserCard key={index} user={user} />;
