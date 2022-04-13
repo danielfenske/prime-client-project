3f9faea5c489a30341166b706a4d39fe30d8ed18
Heather Kim <heatherkim0405@gmail.com>
2022-04-13 11:48:00 -0500

    added req.isAuthenticated

--- server/routes/item.router.js
+++ server/routes/item.router.js
@@ -9,7 +9,7 @@ const router = express.Router();
 router.get('/:item_code', (req, res) => {
     console.log('in item router GET request');
     console.log('req.params is', req.params);
-    
+
     // sqlText = `SELECT * FROM "item" WHERE "item_code" = $1;`;
 
     // pool.query(sqlText, [req.params.id])
@@ -27,22 +27,23 @@ router.post('/', async (req, res) => {
     console.log('in item router POST request');
     console.log('req.body is', req.body);
     const connection = await pool.connect();
+    
+    if (req.isAuthenticated()) {
+        try {
+            await connection.query('BEGIN;');
 
-    try {
-        await connection.query('BEGIN;');
-
-        const sqlText =
-            `INSERT INTO "item" ("item_code", "name", "description", "price_per_price_unit", "unit_type_id")
+            const sqlText =
+                `INSERT INTO "item" ("item_code", "name", "description", "price_per_price_unit", "unit_type_id")
             VALUES ($1, $2, $3, $4, $5)
             RETURNING "id";`;
 
-        const valueArray = [req.body.item_code, req.body.name, req.body.description, req.body.price_per_price_unit, req.body.unit_type_id];
+            const valueArray = [req.body.item_code, req.body.name, req.body.description, req.body.price_per_price_unit, req.body.unit_type_id];
 
-        const newItem = await connection.query(sqlText, valueArray);
-        const newItemId = newItem.rows[0].id;
+            const newItem = await connection.query(sqlText, valueArray);
+            const newItemId = newItem.rows[0].id;
 
-        const sqlTextUnitWeight = 
-        `UPDATE "item" 
+            const sqlTextUnitWeight =
+                `UPDATE "item" 
         SET "unit_weight" = CASE WHEN "unit_type_id" = 1 OR "unit_type_id" = 2 OR "unit_type_id" = 5 OR "unit_type_id" = 7  THEN 1
                                  WHEN "unit_type_id" = 6 THEN 0.01
                                  WHEN "unit_type_id" = 3 OR "unit_type_id" = 8 THEN $1
@@ -50,20 +51,22 @@ router.post('/', async (req, res) => {
                              END
         WHERE "id" = $2;`;
 
-        const valueArrayUnitWeight = [req.body.unit_weight, newItemId];
+            const valueArrayUnitWeight = [req.body.unit_weight, newItemId];
 
-        await connection.query(sqlTextUnitWeight, valueArrayUnitWeight);
+            await connection.query(sqlTextUnitWeight, valueArrayUnitWeight);
 
-        await connection.query('COMMIT;');
-        res.sendStatus(200);
-    } catch (error) {
-        await connection.query('ROLLBACK;');
-        console.log('Transaction Error', error);
-        res.sendStatus(500);
-    } finally {
-        connection.release();
-    }      
-  
+            await connection.query('COMMIT;');
+            res.sendStatus(200);
+        } catch (error) {
+            await connection.query('ROLLBACK;');
+            console.log('Transaction Error', error);
+            res.sendStatus(500);
+        } finally {
+            connection.release();
+        }
+    } else {
+        res.sendStatus(403)
+    }
 });
 
 module.exports = router;
