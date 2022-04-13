951bb45530d7ef37dd120e9a8400e19929092188
Davey-M <davey.meuer@gmail.com>
2022-04-13 13:35:42 -0500

    add Modal to Test component

--- src/components/Test/Test.jsx
+++ src/components/Test/Test.jsx
@@ -1,7 +1,35 @@
 // import components here:
+import { useState } from 'react';
+import Modal from '../Miscellaneous/Modal/Modal';
 
 function Test() {
-  return <>{/* Place components here */}</>;
+  const [open, setOpen] = useState(false);
+
+  return (
+    <>
+      {/* Place components here */}
+      <div>
+        <button
+          onClick={() => {
+            setOpen(true);
+          }}
+        >
+          Open
+        </button>
+      </div>
+
+      <Modal open={open}>
+        <h1>Hello World</h1>
+        <button
+          onClick={() => {
+            setOpen(false);
+          }}
+        >
+          Close
+        </button>
+      </Modal>
+    </>
+  );
 }
 
 export default Test;
