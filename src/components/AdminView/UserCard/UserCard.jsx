e3fb8a350560585254bb64740a2bd178b2b4d764
Davey-M <davey.meuer@gmail.com>
2022-04-13 15:42:30 -0500

    changed input size

--- src/components/AdminView/UserCard/UserCard.jsx
+++ src/components/AdminView/UserCard/UserCard.jsx
@@ -1,17 +1,24 @@
-import { TextField, Select, MenuItem } from '@mui/material';
+import { TextField, Select, MenuItem, Autocomplete } from '@mui/material';
 import { useState } from 'react';
 
 function UserCard({ user }) {
   // destruct the user object
   const { id, username, first_name, last_name, access_level } = user;
 
-  const access_levels = ['', 'Salesperson', 'Sales Lead', 'Administrator'];
+  const access_levels = [
+    // null value is a correction for different starting indexes
+    null, // database starts at index 1 but js starts at index 0
+    'Salesperson',
+    'Sales Lead',
+    'Administrator',
+  ];
 
   const [accessInput, setAccessInput] = useState(access_level);
   const [usernameInput, setUsernameInput] = useState(username);
   const [firstInput, setFirstInput] = useState(first_name);
   const [lastInput, setLastInput] = useState(last_name);
 
+  console.log(accessInput);
   return (
     <>
       <div className='userCard'>
@@ -21,32 +28,36 @@ function UserCard({ user }) {
           label='Username'
           value={usernameInput}
           onChange={(e) => setUsernameInput(e.target.value)}
+          size='small'
         />
 
         <TextField
           label='First Name'
           value={firstInput}
           onChange={(e) => setFirstInput(e.target.value)}
+          size='small'
         />
 
         <TextField
           label='Last Name'
           value={lastInput}
           onChange={(e) => setLastInput(e.target.value)}
+          size='small'
         />
 
         <Select
           value={accessInput}
           onChange={(e) => setAccessInput(e.target.value)}
+          size='small'
         >
           {access_levels.map((lvl, index) => {
-            if (index === 0) return;
-
-            return (
-              <MenuItem key={index} value={index}>
-                {lvl}
-              </MenuItem>
-            );
+            if (index !== 0) {
+              return (
+                <MenuItem key={index} value={index}>
+                  {lvl}
+                </MenuItem>
+              );
+            }
           })}
         </Select>
       </div>
