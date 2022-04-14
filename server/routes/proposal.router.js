bd7afe2b0b708c519f4d6abed51c476235e1ab45
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-04-14 11:58:31 -0500

    Built out GET in proposal router for getting all proposals

--- server/routes/proposal.router.js
+++ server/routes/proposal.router.js
@@ -6,7 +6,74 @@ const {
 } = require('axios');
 
 // #region ==== PROPOSAL ROUTES ====
-    // get single proposal
+
+// get all proposals
+router.get('/', (req, res) => {
+
+    let userId = req.user.id;
+    let access_level = req.user.access_level;
+
+    let queryText;
+    let requestedProposals;
+
+    if (req.isAuthenticated()) {
+        if (access_level > 1) {
+            queryText = `SELECT * FROM "proposal"`
+
+            pool.query(queryText)
+                .then((result) => {
+
+                    let requestedProposals = result.rows;
+
+                    res.sendStatus(requestedProposals);
+                })
+                .catch((error) => {
+                    res.sendStatus(500);
+                })
+
+        } else {
+            queryText = `
+            SELECT 
+            "proposal"."id", 
+            "proposal"."date",
+            "proposal"."proposal_code",
+            "proposal"."opportunity_id",
+            "proposal"."house_type",
+            "proposal"."plan_identifier",
+            "proposal"."plan_date",
+            "proposal"."building_code",
+            "proposal"."partner_discount",
+            "proposal"."surcharge",
+            "proposal"."surcharge_description",
+            "proposal"."method",
+            "proposal"."method_message",
+            "proposal"."delivery_charge",
+            "proposal"."field_weld_charge",
+            "proposal"."field_weld_message",
+            "proposal"."description",
+            "proposal"."disabled"
+            
+            FROM "opportunity"
+            JOIN "proposal" ON "proposal"."opportunity_id" = "opportunity"."id"
+            WHERE "opportunity_id" = 1 AND "opportunity"."user_id" = $1;`
+
+            pool.query(queryText, [userId])
+                .then((result) => {
+
+                    let requestedProposals = result.rows;
+
+                    res.sendStatus(requestedProposals);
+                })
+                .catch((error) => {
+                    res.sendStatus(500);
+                })
+        }
+    } else {
+        res.sendStatus(403);
+    }
+})
+
+// get single proposal
 router.get('/:id', (req, res) => {
 
     let opportunity_id = req.params.id;
