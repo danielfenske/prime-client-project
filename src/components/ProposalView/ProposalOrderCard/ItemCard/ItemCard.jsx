2ec6fabb69486f04daafa592122b69c2a03d7ea5
Davey-M <davey.meuer@gmail.com>
2022-04-14 13:47:55 -0500

    add placeholder data

--- src/components/ProposalView/ProposalOrderCard/ItemCard/ItemCard.jsx
+++ src/components/ProposalView/ProposalOrderCard/ItemCard/ItemCard.jsx
@@ -1,9 +1,39 @@
-function ItemCard() {
+function ItemCard(props) {
   return (
     <>
-      <h1>ItemCard</h1>
+      <div className='item-card'>
+        <h1>ItemCard</h1>
+        {JSON.stringify(props)}
+      </div>
     </>
   );
 }
 
 export default ItemCard;
+
+const itemPlaceholder = [
+  {
+    id: 1,
+    item_code: 'W2765',
+    name: 'Steel Rod',
+    description: '10ft round steel rod',
+    price_per_price_unit: 3.33,
+    unit_type_id: 2,
+  },
+  {
+    id: 2,
+    item_code: 'W2785',
+    name: 'Cookie Wand',
+    description: 'Chocolate Chip... not Oatmeal Raisin',
+    price_per_price_unit: 20.5,
+    unit_type_id: 3,
+  },
+  {
+    id: 3,
+    item_code: 'E3290',
+    name: 'Block of wood',
+    description: "It's a block of wood",
+    price_per_price_unit: 4,
+    unit_type_id: 1,
+  },
+];
