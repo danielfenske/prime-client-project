47bad62a4b93d225b473841c477d88983e06954c
Daniel Fenske <dlfenske.fenske@gmail.com>
2022-04-15 10:45:31 -0500

    Conducted testing for proposal update; moved components from test into ProposalGeneralCard

--- src/components/Test/Test.jsx
+++ src/components/Test/Test.jsx
@@ -6,17 +6,9 @@ import Modal from '../Miscellaneous/Modal/Modal';
 // https://mui.com/material-ui/react-autocomplete/
 
 import { useDispatch } from 'react-redux';
-import TextField from '@mui/material/TextField';
-
 
 function Test() {
-
-  const [name, setName] = useState('');
-  const [email, setEmail] = useState('');
-  const [phone, setPhone] = useState('');
-  const [work_phone, setWorkPhone] = useState('');
-
-  const dispatch = useDispatch();
+const dispatch = useDispatch();
 
 
   const [open, setOpen] = useState(false);
@@ -24,28 +16,6 @@ function Test() {
   const [search, setSearch] = useState('');
 
   const words = ['One', 'Two', 'Three', 'Threees'];
-
-
-  const postProposal = () => {
-    dispatch({ type: 'POST_PROPOSAL', payload: { opportunity_id: 1 } });
-  }
-
-  const deleteProposal = () => {
-    dispatch({type: 'DELETE_PROPOSAL', payload: 23});
-  }
-
-  const handleSubmit = () => {
-    console.log('in handleSubmit');
-
-    let contactSubmission = {
-      name: name,
-      email: email,
-      phone: phone,
-      work_phone: work_phone
-    }
-
-    dispatch({ type: 'POST_CONTACT', payload: contactSubmission });
-  }
 
   return (
     <>
@@ -77,18 +47,6 @@ function Test() {
             })}
         </ul>
       </div>
-
-      <button onClick={postProposal}>Create proposal</button>
-
-      <button onClick={deleteProposal}>Delete proposal</button>
-
-      <form onSubmit={handleSubmit}>
-        <TextField id="outlined-basic" label="Name" variant="outlined" onChange={(e) => setName(e.target.value)} />
-        <TextField id="outlined-basic" label="Email" variant="outlined" onChange={(e) => setEmail(e.target.value)} />
-        <TextField id="outlined-basic" label="Phone" variant="outlined" onChange={(e) => setPhone(e.target.value)} />
-        <TextField id="outlined-basic" label="Work Phone" variant="outlined" onChange={(e) => setWorkPhone(e.target.value)} />
-        <button type="submit">Add</button>
-      </form>
 
       <Modal open={open}>
         <h1>Hello World</h1>
