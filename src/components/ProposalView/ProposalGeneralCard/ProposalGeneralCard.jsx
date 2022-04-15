e5de66250496addabf5ac81077bba670ab9305d8
Davey-M <davey.meuer@gmail.com>
2022-04-15 10:35:11 -0500

    add contact select

--- src/components/ProposalView/ProposalGeneralCard/ProposalGeneralCard.jsx
+++ src/components/ProposalView/ProposalGeneralCard/ProposalGeneralCard.jsx
@@ -4,10 +4,14 @@ import { useDispatch, useSelector } from 'react-redux';
 function ProposalGeneralCard() {
   const dispatch = useDispatch();
   const partners = useSelector((store) => store.partnerReducer.partnerReducer);
+  const contacts = useSelector((store) => store.contactReducer);
 
   useEffect(() => {
     dispatch({
       type: 'FETCH_PARTNER_LIST',
+    });
+    dispatch({
+      type: 'FETCH_CONTACT_LIST',
     });
   }, []);
 
@@ -19,6 +23,7 @@ function ProposalGeneralCard() {
       <div>
         <h2>Partner Information</h2>
         <PartnerCard partners={partners} />
+        <ContactCard contacts={contacts} />
       </div>
       <div>
         <h2>Opportunity Type</h2>
@@ -58,6 +63,42 @@ function PartnerCard({ partners }) {
         </select>
 
         {partnerInfo && <span>{JSON.stringify(partnerInfo)}</span>}
+      </div>
+    </>
+  );
+}
+
+function ContactCard({ contacts }) {
+  const [contactSelect, setContactSelect] = useState(-1);
+  const [contactInfo, setContactInfo] = useState(null);
+
+  useEffect(() => {
+    console.log(contactSelect);
+    if (contactSelect === -1) {
+      setContactInfo(null);
+    } else {
+      setContactInfo(contacts.filter((p) => p.id == contactSelect)[0]);
+    }
+  }, [contactSelect]);
+
+  return (
+    <>
+      <div>
+        <select
+          value={contactSelect}
+          onChange={(e) => setContactSelect(e.target.value)}
+        >
+          <option value={-1}>none</option>
+          {contacts.map((partner, index) => {
+            return (
+              <option key={index} value={partner.id}>
+                {partner.name}
+              </option>
+            );
+          })}
+        </select>
+
+        {contactInfo && <span>{JSON.stringify(contactInfo)}</span>}
       </div>
     </>
   );
